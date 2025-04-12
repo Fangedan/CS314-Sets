0db734a525211135faf9ad379c040be36c10fa41
Andrew Lin <89644316+Fangedan@users.noreply.github.com>
2025-04-12 02:53:25 -0500

    A good night's work.

--- AbstractSet.java
+++ AbstractSet.java
@@ -42,7 +42,6 @@ public abstract class AbstractSet<E> implements ISet<E> {
      * NO METHODS ADDED other than those in ISet and Object.
      */
 
-
     /**
      * Return a String version of this set. 
      * Format is (e1, e2, ... en)
@@ -68,42 +67,287 @@ public abstract class AbstractSet<E> implements ISet<E> {
     }
 
     /**
-     * 
+     * Add an item to this set.
+     * <br> item != null
+     * @param item the item to be added to this set. item may not equal null.
+     * @return true if this set changed as a result of this operation,
+     * false otherwise.
      */
-    public ISet<E> difference(ISet<E> otherSet) {
-        ISet<E> differenceSet = new AbstractSet<>();
+    public boolean add(E item) {
+        // Check Precondition
+        if (item == null) {
+            throw new NullPointerException("Item is null");
+        }
+
+        if (!this.contains(item)) {
+            this.addNew(item);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+    * A union operation. Add all items of otherSet that
+    * are not already present in this set to this set.
+    * @param otherSet != null
+    * @return true if this set changed as a result of this operation,
+    * false otherwise.
+    */
+    public boolean addAll(ISet<E> otherSet) {
+        // Check Precondition
+        if (otherSet == null) {
+            throw new NullPointerException("otherSet is null");
+        }
+
+        Iterator<E> it = otherSet.iterator();
+        boolean changed = false;
+
+        while (it.hasNext()) {
+            E obj = it.next();
+            if (!this.contains(obj)) {
+                this.add(obj);
+                changed = true;
+            }
+        }
+
+        return changed;
+    }
+
+    /**
+     * Make this set empty.
+     * <br>pre: none
+     * <br>post: size() = 0
+     */
+    public void clear() {
         Iterator<E> it = this.iterator();
         
+        while (it.hasNext()) {
+            it.next();
+            it.remove();
+        }
+    }
+
+    /**
+     * Determine if item is in this set.
+     * <br>pre: item != null
+     * @param item element whose presence is being tested.
+     * Item may not equal null.
+     * @return true if this set contains the specified item, false otherwise.
+     */
+    public boolean contains(E item) {
+        // Check Precondition
+        if (item == null) {
+            throw new NullPointerException("Item is null");
+        }
+
+        Iterator<E> it = this.iterator();
+        while (it.hasNext()) {
+            E obj = it.next();
+            if (item.equals(obj)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+   /**
+    * Determine if all of the elements of otherSet are in this set.
+    * <br> pre: otherSet != null
+    * @param otherSet != null
+    * @return true if this set contains all of the elements in otherSet,
+    * false otherwise.
+    */
+    public boolean containsAll(ISet<E> otherSet) {
+        // Check Precondition
+        if (otherSet == null) {
+            throw new NullPointerException("otherSet is null");
+        }
+
+        Iterator<E> it = otherSet.iterator();
+        while (it.hasNext()) {
+            E obj = it.next();
+            if (!this.contains(obj)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+    * Create a new set that is the difference of this set and otherSet.
+    * Return an ISet of elements that are in this Set but not in otherSet.
+     * Also called the relative complement.
+     * <br>Example: If ISet A contains [X, Y, Z] and ISet B contains [W, Z]
+    * then A.difference(B) would return an ISet with elements [X, Y] while
+    * B.difference(A) would return an ISet with elements [W].
+    * <br>pre: otherSet != null
+    * <br>post: returns a set that is the difference of this set and otherSet.
+    * Neither this set or otherSet are altered as a result of this operation.
+    * @param otherSet != null
+    * @return a set that is the difference of this set and otherSet
+    */
+    public ISet<E> difference(ISet<E> otherSet){
+        // Check Precondition
+        if (otherSet == null) {
+            throw new NullPointerException("otherSet is null");
+        }
+
+        ISet<E> differenceSet = new UnsortedSet<>();
+        Iterator<E> it = this.iterator();      
         while (it.hasNext()) {
             E obj = it.next();
             if (!otherSet.contains(obj)) {
                 differenceSet.add(obj);
             }
         }
+
         return differenceSet;
     }
 
-    public ISet<E> union(ISet<E> otherSet){
-        ISet<E> unionSet = otherSet;
-        ISet<E> differenceSet = difference(otherSet);
-        Iterator<E> it = differenceSet.iterator();
-        while (it.hasNext()){
-            E obj = it.next();
-            unionSet.add(obj);
+    /**
+    * Determine if this set is equal to other.
+    * Two sets are equal if they have exactly the same elements.
+    * The order of the elements does not matter.
+    * pre: none
+    * @param other the object to compare to this set
+    * @return true if other is a Set and has the same elements as this set
+    */
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;  // Same object reference
         }
-        return unionSet;
+    
+        if (other == null || !(other instanceof ISet<?>)) {
+            return false;  // Different type or null
+        }
+    
+        ISet<?> otherSet = (ISet<?>) other;
+    
+        // Check if the sets have the same number of elements
+        if (this.size() != otherSet.size()) {
+            return false;
+        }
+    
+        // Check if all elements in this set are contained in the other set
+        Iterator<E> it = this.iterator();
+        while (it.hasNext()) {
+            E item = it.next();
+            if (!otherSet.contains(item)) {
+                return false;  // Found an element not in the other set
+            }
+        }
+    
+        return true;  // All elements are present in both sets
     }
 
     /**
-     * 
-     */
-    public boolean equals(Object other){
-        
+    * Create a new set that is the intersection of this set and otherSet.
+    * <br>pre: otherSet != null<br>
+    * <br>post: returns a set that is the intersection of this set
+    * and otherSet.
+    * Neither this set or otherSet are altered as a result of this operation.
+    * @param otherSet != null
+    * @return a set that is the intersection of this set and otherSet
+    */
+    public ISet<E> intersection(ISet<E> otherSet){
+        // Check Precondition
+        if (otherSet == null) {
+            throw new NullPointerException("otherSet is null");
+        }
+
+        ISet<E> intersectionSet = new UnsortedSet<>();
+        Iterator<E> it = this.iterator();
+        while (it.hasNext()) {
+            E obj = it.next();
+            if (otherSet.contains(obj)) {
+                intersectionSet.add(obj);
+            }
+        }
+
+        return intersectionSet;
     }
 
-    public boolean add(E item){
-        if (!this.contains(item)){
-            
+    /**
+     * Return an Iterator object for the elements of this set.
+     * pre: none
+     * @return an Iterator object for the elements of this set
+     * Abstract method for iterators - to be implemented by subclasses
+     */
+    public abstract Iterator<E> iterator();
+
+    /**
+    * Remove the specified item from this set if it is present.
+    * pre: item != null
+    * @param item the item to remove from the set. item may not equal null.
+    * @return true if this set changed as a result of this operation,
+    * false otherwise
+    */
+    public boolean remove(E item) {
+        // Check Precondition
+        if (item == null) {
+            throw new NullPointerException("Item is null");
         }
+
+        boolean changed = false;
+        Iterator<E> it = this.iterator();
+        while (it.hasNext()) {
+            E obj = it.next();
+            if (obj.equals(item)) {
+                it.remove();
+                changed = true;
+            }
+        }
+
+        return changed;
+    }
+
+    /**
+    * Return the number of elements of this set.
+    * pre: none
+    * @return the number of items in this set
+    */
+    public int size() {
+        Iterator<E> it = this.iterator();
+        int count = 0;
+        while (it.hasNext()) {
+            it.next();
+            count++;
+        }
+
+        return count;
+    }
+
+    /**
+    * Create a new set that is the union of this set and otherSet.
+    * <br>pre: otherSet != null
+    * <br>post: returns a set that is the union of this set and otherSet.
+    * Neither this set or otherSet are altered as a result of this operation.
+    * @param otherSet != null
+    * @return a set that is the union of this set and otherSet
+    */
+    public ISet<E> union(ISet<E> otherSet){
+        // Check Precondition
+        if (otherSet == null) {
+            throw new NullPointerException("otherSet is null");
+        }
+
+        ISet<E> unionSet = new UnsortedSet<>();
+
+        // Add all elements from this set
+        Iterator<E> it1 = this.iterator();
+        while (it1.hasNext()) {
+            unionSet.add(it1.next());
+        }
+
+        // Add all elements from otherSet (will skip duplicates if set handles them)
+        Iterator<E> it2 = otherSet.iterator();
+        while (it2.hasNext()) {
+            unionSet.add(it2.next());
+        }
+
+        return unionSet;
     }
 }
--- UnsortedSet.java
+++ UnsortedSet.java
@@ -33,4 +33,60 @@ public class UnsortedSet<E> extends AbstractSet<E> {
 
     private ArrayList<E> myCon;
 
+    // Constructor for initializing the container
+    public UnsortedSet() {
+        myCon = new ArrayList<>();
+    }
+
+    /**
+     * Add an item to this set.
+     * <br> item != null
+     * @param item the item to be added to this set. item may not equal null.
+     * @return true if this set changed as a result of this operation,
+     * false otherwise.
+     */
+    public boolean add(E item) {
+        // Check Precondition
+        if (item == null) {
+            throw new NullPointerException("Item is null");
+        }
+
+        if (!myCon.contains(item)) {
+            myCon.add(item);
+            return true;
+        }
+        
+        return false;
+    }
+
+
+    /**
+     * Return an Iterator object for the elements of this set.
+     * pre: none
+     * @return an Iterator object for the elements of this set
+     */
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int currentIndex = 0;
+
+            // Check if there are more elements in the container
+            public boolean hasNext() {
+                return currentIndex < myCon.size();
+            }
+
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                // Return the next element and move the index
+                return myCon.get(currentIndex++);
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException("Remove" 
+                 + "operation is not supported.");
+            }
+        };
+    }
+
 }
